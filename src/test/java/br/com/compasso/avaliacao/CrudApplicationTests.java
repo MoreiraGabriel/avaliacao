ff9cd5d7c9400bb01a11683f4140a55fe77e7dbc
Gabriel Moreira <gabriel.moreira@novatendencia.localdomain>
2021-02-12 00:58:26 -0300

    Primeiro commit

---
+++ src/test/java/br/com/compasso/avaliacao/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.compasso.avaliacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
