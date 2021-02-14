b25a61990bdccef5a2733472a5a35f0a22e8814b
Gabriel Moreira <gabriel.moreira@novatendencia.localdomain>
2021-02-14 03:14:04 -0300

    Adicionando Swagger.

--- src/main/java/br/com/compasso/avaliacao/CrudApplication.java
+++ src/main/java/br/com/compasso/avaliacao/CrudApplication.java
@@ -3,7 +3,10 @@ package br.com.compasso.avaliacao;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class CrudApplication {
 
 	public static void main(String[] args) {
